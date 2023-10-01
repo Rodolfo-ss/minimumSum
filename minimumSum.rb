6684b91c81df3c7858c114fa002c9d5510860b9e
unknown <rodolfcs@gmail.com>
2023-10-01 11:11:08 -0300

    minimum

---
+++ minimumSum.rb
@@ -0,0 +1,16 @@
+linguageArray = ["java", "C++", "Ruby", "C", "Jest", "React"]
+numberArray = [12, 3, 45, 76, 44, 99]
+letterArray = ['a', 'e', 'o', 'p', 'r', 'm']
+animals = ['cat', 'camel', 'dog', 'cow', 'bat', 'elephant', 'ramster']
+
+
+a = linguageArray.min()
+b = numberArray.min()
+c = letterArray.min()
+d = animals.min()
+
+puts "#{linguageArray} o menor = #{a}"
+puts "#{numberArray} o menor = #{b}"
+puts "#{letterArray} o menor = #{c}"
+puts "#{animals} o menor = #{d}"
+
